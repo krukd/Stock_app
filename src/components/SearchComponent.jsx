4e308a43e9d9992d8110742ae25cb6264b173aae
krukd <krukd87@gmail.com>
2024-08-24 15:50:50 +0800

    Search component part 1

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,3 +1,15 @@
 export const SearchComponent = () => {
-    return <div>Search</div>
-}
+  return (
+    <div className="w-50 p-5 rounded mx-auto">
+      <div className="form-floating dropdown">
+        <input
+          style={{ backgroundColor: "rgba(145, 158, 171, 0.04)" }}
+          id="search"
+          type="text"
+          className="form-control"
+          placeholder="Search"
+        />
+      </div>
+    </div>
+  );
+};
